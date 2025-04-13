986591afeda029e05ae076be4474843959f0aaba
POORANI-B-3003 <113784719+POORANI-B-3003@users.noreply.github.com>
2025-04-13 13:46:25 +0530

    Add files via upload

---
+++ Assignments/Java Task 4/src/com/hexaware/sis/exception/InvalidPaymentException.java
@@ -0,0 +1,7 @@
+package com.hexaware.sis.exception;
+
+public class InvalidPaymentException extends Exception {
+    public InvalidPaymentException(String message) {
+        super(message);
+    }
+}
