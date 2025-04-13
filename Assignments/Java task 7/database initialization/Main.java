a4552b7868c4893fcb1948bed4ff1fee718a3bb8
POORANI-B-3003 <113784719+POORANI-B-3003@users.noreply.github.com>
2025-04-13 21:49:34 +0530

    Delete Assignments/Java task 7/database initialization directory

--- Assignments/Java task 7/database initialization/Main.java
+++
@@ -1,9 +0,0 @@
-package sis;
-
-import com.hexaware.sis.db.DatabaseInitializer;
-
-public class Main {
-    public static void main(String[] args) {
-        DatabaseInitializer.initialize();
-    }
-}
