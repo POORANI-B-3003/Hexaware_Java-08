1e31a5281e90cd03740df35a26d9b76d372d1f09
POORANI-B-3003 <113784719+POORANI-B-3003@users.noreply.github.com>
2025-04-13 21:51:10 +0530

    Create Main.java

---
+++ Assignments/Java task 7/7.1 Database Initialization/Main.java
@@ -0,0 +1,9 @@
+package sis;
+
+import com.hexaware.sis.db.DatabaseInitializer;
+
+public class Main {
+    public static void main(String[] args) {
+        DatabaseInitializer.initialize();
+    }
+}
