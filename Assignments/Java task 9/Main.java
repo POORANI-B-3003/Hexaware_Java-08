a8e15de683959c02a928a33a8959d492226300db
POORANI-B-3003 <113784719+POORANI-B-3003@users.noreply.github.com>
2025-04-14 22:48:47 +0530

    Delete Assignments/Java task 9 directory

--- Assignments/Java task 9/Main.java
+++
@@ -1,9 +0,0 @@
-package com.hexaware.sis.main;
-
-import com.hexaware.sis.db.TeacherAssignmentService;
-
-public class Main {
-    public static void main(String[] args) {
-        TeacherAssignmentService.assignTeacherToCourse();
-    }
-}
