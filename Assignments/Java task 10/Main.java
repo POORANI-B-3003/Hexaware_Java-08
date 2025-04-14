b3381f7ff7f8b2a13c65348422e095277d106599
POORANI-B-3003 <113784719+POORANI-B-3003@users.noreply.github.com>
2025-04-14 22:00:34 +0530

    Create Main.java

---
+++ Assignments/Java task 10/Main.java
@@ -0,0 +1,9 @@
+package sis;
+
+import com.hexaware.sis.db.PaymentService;
+
+public class Main {
+    public static void main(String[] args) {
+        PaymentService.recordJanePayment();
+    }
+}
