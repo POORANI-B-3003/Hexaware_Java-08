f1b0ffafab35ad7c9d2b0e03e07e1f2598162e73
POORANI-B-3003 <113784719+POORANI-B-3003@users.noreply.github.com>
2025-04-14 22:48:27 +0530

    Delete Assignments/Java task 7 directory

--- Assignments/Java task 7/7.1 Database Initialization/Main.java
+++
@@ -1,9 +0,0 @@
-package com.hexaware.sis.main;
-
-import com.hexaware.sis.db.DatabaseInitializer;
-
-public class Main {
-    public static void main(String[] args) {
-        DatabaseInitializer.initialize();
-    }
-}
--- Assignments/Java task 7/7.5 Dynamic Query Builder/Main.java
+++
@@ -1,9 +0,0 @@
-package com.hexaware.sis.main;
-
-import com.hexaware.sis.db.DynamicQueryBuilder;
-
-public class Main {
-    public static void main(String[] args) {
-        DynamicQueryBuilder.runCustomQuery();
-    }
-}
