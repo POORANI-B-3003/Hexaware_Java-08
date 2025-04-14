a58b0d7bad012d8068405f5e7dea6fdd4c6d3ecb
POORANI-B-3003 <113784719+POORANI-B-3003@users.noreply.github.com>
2025-04-14 22:47:22 +0530

    Delete Assignments/Java task 10 directory

--- Assignments/Java task 10/Main.java
+++
@@ -1,9 +0,0 @@
-package com.hexaware.sis.main;
-
-import com.hexaware.sis.db.PaymentService;
-
-public class Main {
-    public static void main(String[] args) {
-        PaymentService.recordJanePayment();
-    }
-}
