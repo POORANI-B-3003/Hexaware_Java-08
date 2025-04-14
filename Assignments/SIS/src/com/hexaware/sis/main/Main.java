33fefc2337241c048da218a0baa592a02d889783
POORANI-B-3003 <113784719+POORANI-B-3003@users.noreply.github.com>
2025-04-14 22:46:50 +0530

    Delete Assignments/SIS directory

--- Assignments/SIS/src/com/hexaware/sis/main/Main.java
+++
@@ -1,9 +0,0 @@
-package com.hexaware.sis.main;
-
-import com.hexaware.sis.db.EnrollmentReportService;
-
-public class Main {
-    public static void main(String[] args) {
-        EnrollmentReportService.generateEnrollmentReport();
-    }
-}
