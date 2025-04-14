a0eebadf79c381e08e7b0145601415c5b071854d
POORANI-B-3003 <113784719+POORANI-B-3003@users.noreply.github.com>
2025-04-14 22:48:37 +0530

    Delete Assignments/Java task 8 directory

--- Assignments/Java task 8/Main.java
+++
@@ -1,9 +0,0 @@
-package com.hexaware.sis.main;
-
-import com.hexaware.sis.db.StudentEnrollmentService;
-
-public class Main {
-    public static void main(String[] args) {
-        StudentEnrollmentService.enrollJohnDoe();
-    }
-}
