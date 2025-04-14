9701013c56b887c7cbf2f4a14ef3495f5231c90a
POORANI-B-3003 <113784719+POORANI-B-3003@users.noreply.github.com>
2025-04-14 22:04:49 +0530

    Create Main.java

---
+++ Assignments/Java task 11/Main.java
@@ -0,0 +1,9 @@
+package sis;
+
+import com.hexaware.sis.db.EnrollmentReportService;
+
+public class Main {
+    public static void main(String[] args) {
+        EnrollmentReportService.generateEnrollmentReport();
+    }
+}
