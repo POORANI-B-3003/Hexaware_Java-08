40628acfc8c061e14e676b06bd1a08ae7036539e
POORANI-B-3003 <113784719+POORANI-B-3003@users.noreply.github.com>
2025-04-14 21:52:52 +0530

    Create Main.java

---
+++ Assignments/Java task 9/Main.java
@@ -0,0 +1,9 @@
+package sis;
+
+import com.hexaware.sis.db.TeacherAssignmentService;
+
+public class Main {
+    public static void main(String[] args) {
+        TeacherAssignmentService.assignTeacherToCourse();
+    }
+}
