5fe22645a0d52d19071913d45ef7d214f097a047
POORANI-B-3003 <113784719+POORANI-B-3003@users.noreply.github.com>
2025-04-15 06:21:07 +0530

    Delete Student Information System (SIS) directory

--- Assignments/Student Information System (SIS)/src/com/hexaware/sis/exception/TeacherNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.hexaware.sis.exception;
-
-public class TeacherNotFoundException extends Exception {
-    public TeacherNotFoundException(String message) {
-        super(message);
-    }
-}
